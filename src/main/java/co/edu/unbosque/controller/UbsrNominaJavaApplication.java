c881b223e7a00539bbd485bf4f78ba391d539f02
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-05-24 11:47:46 -0500

    Initial commit | Gabriel Blanco

---
+++ src/main/java/co/edu/unbosque/controller/UbsrNominaJavaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsrNominaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbsrNominaJavaApplication.class, args);
+    }
+
+}
