f20cd66c20f12b48c5f601deebc67ed7febc0323
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-06-01 12:34:26 -0500

    ExcelitoProcessor recibe los datos separados por coma independiente del idioma. | Gabriel Blanco

--- src/main/java/co/edu/unbosque/model/persistence/utils/DatabaseConnection.java
+++ src/main/java/co/edu/unbosque/model/persistence/utils/DatabaseConnection.java
@@ -1,4 +1,5 @@
 package co.edu.unbosque.model.persistence.utils;
 
 public class DatabaseConnection {
+    //TODO: @colgaben96 porfis realizar los queries
 }
--- src/main/java/co/edu/unbosque/model/persistence/utils/ExcelitoProcessor.java
+++ src/main/java/co/edu/unbosque/model/persistence/utils/ExcelitoProcessor.java
@@ -1,5 +1,23 @@
 package co.edu.unbosque.model.persistence.utils;
 
+import java.io.*;
+
 public class ExcelitoProcessor {
+
+    private File excelito;
+
+    public ExcelitoProcessor(File excelito) {
+        this.excelito = excelito;
+    }
+
+    public String[] read() throws IOException, ClassNotFoundException {
+        FileReader fr = new FileReader(excelito);
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();
+        while(line != null) {
+            line += br.readLine();
+        }
+        return line.contains(",") ? line.split(",") : line.split(";");
+    }
 
 }
