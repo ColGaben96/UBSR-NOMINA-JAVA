6167eed0ab6ab500459136784c78cb5f80a49e56
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-06-14 00:38:59 -0500

    Html pre config| Gabriel Blanco

--- src/main/java/co/edu/unbosque/controller/Controller.java
+++ src/main/java/co/edu/unbosque/controller/Controller.java
@@ -1,12 +1,23 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.model.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
+    @Autowired
+    private UsuarioService usuario;
+
     @GetMapping("/")
     public String goIndex() {
         return "index";
     }
+    @GetMapping("/Aplicacion/login")
+    public String goLogin() { return "app/login"; }
+    @GetMapping("/Aplicacion/")
+    public String goDashboard() { return "app/dashboard"; }
+    @GetMapping("/Aplicacion/signup")
+    public String goSignup() { return "app/signup"; }
 }
