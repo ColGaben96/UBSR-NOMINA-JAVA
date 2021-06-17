436cf21e4cd6b429965914c79a3be963101e67cd
Gabriel Blanco <geblarot96@gmail.com>
2021-06-17 16:58:22 -0500

    Done Database Definition | Gabriel Blanco

--- src/main/java/co/edu/unbosque/model/persistence/ConceptoNominaDTO.java
+++ src/main/java/co/edu/unbosque/model/persistence/ConceptoNominaDTO.java
@@ -1,9 +1,14 @@
 package co.edu.unbosque.model.persistence;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
+@Data
+@Table(name = "conceptonomina")
 public class ConceptoNominaDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 613202106L;
