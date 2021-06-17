9605eb6ac1179f9c369ed9c211e6f1ad90167796
Gabriel Blanco <geblarot96@gmail.com>
2021-06-17 17:06:44 -0500

    Linked Service. With this backend should be complete except for utils package | Gabriel Blanco

--- src/main/java/co/edu/unbosque/controller/Controller.java
+++ src/main/java/co/edu/unbosque/controller/Controller.java
@@ -1,12 +1,34 @@
 package co.edu.unbosque.controller;
 
-import co.edu.unbosque.model.service.UsuarioService;
+import co.edu.unbosque.model.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
+    @Autowired
+    private AccesoService acceso;
+    @Autowired
+    private ARLService arl;
+    @Autowired
+    private CargoService cargo;
+    @Autowired
+    private ConceptoNominaService concepto;
+    @Autowired
+    private DepartamentoService departamento;
+    @Autowired
+    private EmpleadoService empleado;
+    @Autowired
+    private EmpresaService empresa;
+    @Autowired
+    private EPSService eps;
+    @Autowired
+    private NovedadService novedad;
+    @Autowired
+    private RolService rol;
+    @Autowired
+    private SucursalService sucursal;
     @Autowired
     private UsuarioService usuario;
 
